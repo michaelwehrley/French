fa3b8787d6ecb7e2b37460f272ea52a52ad60048
Michael Wehrley <michaelwehrley@gmail.com>
2019-10-12 12:52:08 -0500

    Work Offline and fix number bug.

---
+++ play.js
@@ -0,0 +1,75 @@
+(function(app) {
+  "use strict";
+
+  function answerText(answers) {
+    return (answers[0] + (answers[1] ? (" (" + answers[1] + ")") : ""));
+  }
+
+  function clearAnswer() {
+    $("#answer").val("");
+  }
+
+  function disableAutoComplete() {
+    $("#answer").prop("name", Math.random());
+  }
+
+  function displayQuestion(currentQuestion, answers) {
+    $("#question").html(currentQuestion);
+  }
+
+  function isCorrect(answers) {
+    return $("#answer").val().trim().toLowerCase() === answers[0].toLowerCase();
+  }
+
+  function play(currentQuestion) {
+    var answers = app.numbers[Number(currentQuestion)];
+
+    displayQuestion(currentQuestion, answers);
+    clearAnswer();
+    disableAutoComplete();
+    skipQuestion();
+
+    $("form#submit-answer").off("submit").on("submit", function(event) {
+      event.preventDefault();
+      $(".help-block").html(answerText(answers));
+
+      if (isCorrect(answers)) {
+        $(".alert-danger").addClass("d-none");
+        $("button#submit").addClass("d-none");
+        $(".alert-success").removeClass("d-none");
+        $("a#next").removeClass("d-none");
+        nextQuestion();
+      } else {
+        $(".alert-danger").removeClass("d-none");
+        $(".alert-success").addClass("d-none");
+      }
+    });
+  }
+
+  function nextQuestion() {
+    $("a#next").off("click").on("click", function(event) {
+      event.preventDefault();
+
+      reset();
+      play(app.randomNumber());
+    });
+  }
+
+  function reset() {
+    $("a#next").addClass("d-none");
+    $("button#submit").removeClass("d-none");
+    $(".alert-success").addClass("d-none");
+    $(".alert-danger").addClass("d-none");
+  }
+
+  function skipQuestion() {
+    $("a#skip").off("click").on("click", function(event) {
+      event.preventDefault();
+
+      reset();
+      play(app.randomNumber());
+    });
+  }
+
+  app.play = play;
+}(FrenchApp));
